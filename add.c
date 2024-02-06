d3b160b6e044a96fa3f71969759991ee15d02860
Rohiteshvar S B <140417032+Rohiteshvar@users.noreply.github.com>
2024-02-06 14:02:15 +0530

    Update diff.c
    add and sub from 2 numbers

--- add.c
+++ add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
+    int num1, num2, sum , diff;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -10,8 +10,10 @@ int main() {
     scanf("%d", &num2);
     
     sum = num1 + num2;
+    diff= num2 - num1;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The diff of %d and %d is %d", num1,num2,diff);
     
     return 0;
 }
